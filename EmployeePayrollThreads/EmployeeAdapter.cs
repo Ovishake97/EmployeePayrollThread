6e651986e4e9afcd0f268d485d24a91a69149eea
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-11-12 14:17:34 +0530

    [Abhishek] added to UC5 branch

--- EmployeePayrollThreads/EmployeeAdapter.cs
+++ EmployeePayrollThreads/EmployeeAdapter.cs
@@ -10,7 +10,7 @@ namespace EmployeePayrollThreads
 {
     public class EmployeeAdapter
     {
-        
+        private object locker = new object();
         /// Defining a function to add an employee object to 
         /// the sql table
         public void AddEmployee(EmployeeModels employee)
@@ -69,5 +69,96 @@ namespace EmployeePayrollThreads
                     thread.Join();
                 });
             }
+        /// Method to add salary and employee to two separate tables
+        /// using transaction, thread and locker
+        public void AddEmployeeAndSalary(List<EmployeeDetail> employeeList,List<SalaryModel> salaryList)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB; Initial Catalog =payroll_ado; User ID = AkSharma; Password=abhishek123";
+         SqlConnection connection = new SqlConnection(connectionString);
+            using (connection)
+            {
+                connection.Open();
+                SqlTransaction sqlTran = connection.BeginTransaction();
+                SqlCommand command = connection.CreateCommand();
+                command.Transaction = sqlTran;
+
+                try
+                {
+                    //Locking a thread with the help of lockers 
+                    //So one thread executes at one time
+                    lock (locker) {
+                        Thread thread = new Thread(() =>
+                        {
+                            Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " starting");
+                            salaryList.ForEach(salary =>
+                            {
+
+                                Console.WriteLine("Salary adding " + salary.SalaryId);
+                                this.AddSalaryTransaction(salary);
+                                Console.WriteLine(" Salary added: " + salary.SalaryId);
+
+                            }
+                        );
+                            employeeList.ForEach(employeeData =>
+
+                            {
+
+                                Console.WriteLine("Employee adding: " + employeeData.employeeName);
+                                this.AddEmployeeTransaction(employeeData);
+                                Console.WriteLine(" Employee added: " + employeeData.employeeName);
+
+
+                            }
+                        );
+                            Console.WriteLine(Thread.CurrentThread.ManagedThreadId + " ended");
+
+                        });
+                        thread.Start();
+                        thread.Join();
+                        sqlTran.Commit();
+                    }
+                    
+                    
+                    Console.WriteLine("Both records were written to database."); 
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    try
+                    {
+                        sqlTran.Rollback();
+                    }
+                    catch (Exception exRollback)
+                    {
+                        Console.WriteLine(exRollback.Message);
+                    }
+                    
+                }
+            }
+        }
+        /// Method to add employee for UC5
+        public void AddEmployeeTransaction(EmployeeDetail employee) {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB; Initial Catalog =payroll_ado; User ID = AkSharma; Password=abhishek123";
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (connection) {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText =
+                "insert into Employee_Payroll(name,gender,salaryid) values('" + employee.employeeName + "','" + employee.gender + "'," + employee.salaryID + ")";
+                command.ExecuteNonQuery();
+            }
+        }
+        /// Method to add salaries for UC5
+        public void AddSalaryTransaction(SalaryModel salary) {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB; Initial Catalog =payroll_ado; User ID = AkSharma; Password=abhishek123";
+            SqlConnection connection = new SqlConnection(connectionString);
+            using (connection)
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = "insert into Payroll_Details(salaryid,salary) values(" + salary.SalaryId + "," + salary.basicPay + ")";
+                command.ExecuteNonQuery();
+            }
+        }
     }
 }
---
+++ EmployeePayrollThreads/EmployeeDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollThreads
+{
+    public class EmployeeDetail
+    {
+        public EmployeeDetail(int employeeID, string employeeName, string gender, int salaryID)
+        {
+            this.employeeID = employeeID;
+            this.employeeName = employeeName;
+            this.gender = gender;
+            this.salaryID = salaryID;
+        }
+
+        public int employeeID { get; set; }
+        public string employeeName { get; set; }
+        public string gender { get; set; }
+        public int salaryID { get; set; }
+    }
+}
